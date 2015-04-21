4181bf5955215ddca3d44add58c813bab8a4c042
Alejandro Escobar <alejoescobac@gmail.com>
2015-04-21 10:36:36 -0500

    Add rake db:seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  username   :string
+#  email      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |user|
+  User.create(username: Faker::Internet.user_name, email: Faker::Internet.safe_email, name: Faker::Name.name)
+end
+
+20.times do |article|
+  Article.create(title: Faker::Name.title, body: Faker::Hacker.say_something_smart, user_id: rand(1..10))
+end
+
+30.times do |comment|
+  Comment.create(body: Faker::Hacker.say_something_smart, user_id: rand(1..10), article_id: rand(1..20))
+end
